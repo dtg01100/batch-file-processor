1c878b244aaaa3cf29c0544a01c33e62f31b3dd5
dtg01100 <dlafreniere349@gmail.com>
2016-04-12 11:22:38 -0400

    split number to letters library out. todo: stop using bundled version of number_to_letters when library is added to pypi

---
+++ number_to_letters.py
@@ -0,0 +1,38 @@
+def generate_letters(number_from_input):
+    number = int(number_from_input)
+    if number < 0:
+        raise Exception("Input number needs to be positive integer")
+    numbers_list = []
+    _number = number
+    if _number == 0:
+        numbers_list.append(number)
+    while _number != 0:
+        quotient, remainder = divmod(_number, 26)
+        _number = quotient
+        if len(numbers_list) > 0 and quotient == 0:
+            number_to_insert = remainder - 1
+        else:
+            number_to_insert = remainder
+        numbers_list.insert(0, number_to_insert)
+    letter_list = []
+    for letter_number in numbers_list:
+        letter = chr(ord('A') + letter_number)
+        letter_list.append(letter)
+    final_string = "".join(letter_list)
+    return final_string
+
+if __name__ == "__main__":
+    number_is_good = False
+    input_number = 0
+    while not number_is_good:
+        input_number = input("Input number to convert into letters: ")
+        try:
+            input_number = int(input_number)
+            if input_number < 0:
+                print("Input number needs to be a positive integer")
+            else:
+                number_is_good = True
+        except ValueError:
+            print("Input was not an integer")
+    output = generate_letters(input_number)
+    print("Letter Sequence is:", output)
--- split_edi.py
+++ split_edi.py
@@ -1,25 +1,8 @@
 import os
+import number_to_letters
 
 
 def do_split_edi(edi_process, work_directory):
-
-    def _generate_prepend_letter(number):
-        numbers_list = []
-        _number = number
-        while _number != 0:
-            quotient, remainder = divmod(_number, 26)
-            _number = quotient
-            if len(numbers_list) > 0 and quotient == 0:
-                number_to_insert = remainder - 1
-            else:
-                number_to_insert = remainder
-            numbers_list.insert(0, number_to_insert)
-        letters_list = []
-        for letter_number in numbers_list:
-            letter = chr(ord('A') + letter_number)
-            letters_list.append(letter)
-        final_string = "".join(letters_list)
-        return final_string
 
     f = None
     output_file_path = None
@@ -39,19 +22,20 @@ def do_split_edi(edi_process, work_directory):
         for line_mum, line in enumerate(work_file_lined):  # iterate over work file contents
             writeable_line = line
             if writeable_line.startswith("A"):
+                prepend_letters = number_to_letters.generate_letters(count)
                 count += 1
                 try:
                     f.close()
                 except Exception:
                     pass
                 edi_send_list.append(output_file_path)
-                prepend_letters = _generate_prepend_letter(count)
                 output_file_path = os.path.join(work_directory, prepend_letters + " " + os.path.basename(edi_process))
                 f = open(output_file_path, 'wb')
             f.write(writeable_line.replace('\n', "\r\n"))
         f.close()  # close output file
         edi_send_list.append(output_file_path)
         edi_send_list.pop(0)
+        assert len(edi_send_list) == list_of_first_characters.count("A")
         if len(edi_send_list) < 1:
             raise Exception("No Split EDIs")
     return edi_send_list
