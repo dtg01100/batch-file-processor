f82ccbc78f091f762cf6f7dd955d3de5c859c9e5
dtg01100 <dlafreniere349@gmail.com>
2016-04-13 12:36:28 -0400

    ensure that split edi files have the same number of lines as the input edi

--- split_edi.py
+++ split_edi.py
@@ -7,10 +7,13 @@ def do_split_edi(edi_process, work_directory):
     f = None
     output_file_path = None
     count = 0
+    write_counter = 0
     edi_send_list = []
     if not os.path.exists(work_directory):
         os.mkdir(work_directory)
     with open(edi_process) as work_file:  # open input file
+        lines_in_edi = sum(1 for _ in work_file)
+        work_file.seek(0)
         work_file_lined = [n for n in work_file.readlines()]  # make list of lines
         list_of_first_characters = []
         for line in work_file_lined:
@@ -24,18 +27,26 @@ def do_split_edi(edi_process, work_directory):
             if writeable_line.startswith("A"):
                 prepend_letters = number_to_letters.generate_letters(count)
                 count += 1
-                try:
+                if not len(edi_send_list) == 0:
                     f.close()
-                except Exception:
-                    pass
                 edi_send_list.append(output_file_path)
                 output_file_path = os.path.join(work_directory, prepend_letters + " " + os.path.basename(edi_process))
                 f = open(output_file_path, 'wb')
             f.write(writeable_line.replace('\n', "\r\n"))
+            write_counter += 1
         f.close()  # close output file
         edi_send_list.append(output_file_path)
         edi_send_list.pop(0)
-        assert len(edi_send_list) == list_of_first_characters.count("A")
+        edi_send_list_lines = 0
+        for output_file in edi_send_list:
+            with open(output_file) as file_handle:
+                edi_send_list_lines += sum(1 for _ in file_handle)
+        if not lines_in_edi == write_counter:
+            raise Exception("not all lines in input were written out")
+        if not lines_in_edi == edi_send_list_lines:
+            raise Exception("total lines in output files do not match input file")
+        if not len(edi_send_list) == list_of_first_characters.count("A"):
+            raise Exception('mismatched number of "A" records')
         if len(edi_send_list) < 1:
             raise Exception("No Split EDIs")
     return edi_send_list
