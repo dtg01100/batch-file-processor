5188c4210974bffeaab1d2544dd465bea941dbaa
dtg01100 <dlafreniere349@gmail.com>
2016-04-20 13:28:08 -0400

    backups now have date and time appended to the filename

--- backup_increment.py
+++ backup_increment.py
@@ -1,11 +1,19 @@
 import shutil
 import os
+import time
 
 
 def do_backup(input_file):
-    counter = 0
-    while os.path.exists(input_file + '.bak' + str(counter)):
-        counter += 1
-    backup_path = input_file + '.bak' + str(counter)
+    counter = 1
+    proposed_backup_folder_path = os.path.join(os.path.dirname(input_file), "backups")
+    if os.path.isdir(proposed_backup_folder_path) or not os.path.exists(proposed_backup_folder_path):
+        backup_folder_path = proposed_backup_folder_path
+    else:
+        while os.path.isfile(proposed_backup_folder_path + str(counter)):
+            counter += 1
+        backup_folder_path = proposed_backup_folder_path + str(counter)
+    if not os.path.exists(backup_folder_path):
+        os.mkdir(backup_folder_path)
+    backup_path = os.path.join(backup_folder_path, input_file + '.bak' + "-" + str(time.ctime()).replace(":", "-"))
     shutil.copy(input_file, backup_path)
     return backup_path
