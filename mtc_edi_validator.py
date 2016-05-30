78d385172b6ef53ef29c4bf47b0ba2d3f4355e0f
dtg01100 <dlafreniere349@gmail.com>
2016-05-30 09:07:37 -0400

    revert last two commits

--- mtc_edi_validator.py
+++ mtc_edi_validator.py
@@ -58,7 +58,4 @@ def report_edi_issues(input_file):
                 in_memory_log.write("line is:\r\n")
                 in_memory_log.write(line)
                 _insert_description_and_number(line)
-    else:
-        has_errors = True
-        in_memory_log.write(input_file + " is not a valid edi file\r\n\r\n")
     return in_memory_log, has_errors
