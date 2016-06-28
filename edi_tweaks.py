4d7d844bea6fff59e916eba3b46a2f7bc60cc0c0
dtg01100 <dlafreniere349@gmail.com>
2016-06-28 15:51:04 -0400

    ensure that edi tweaks have correct line endings

--- edi_tweaks.py
+++ edi_tweaks.py
@@ -1,13 +1,13 @@
 def edi_tweak(edi_process, output_filename, pad_arec, arec_padding):
     work_file = open(edi_process)  # open input file
     work_file_lined = [n for n in work_file.readlines()]  # make list of lines
-    f = open(output_filename, 'w')  # open work file, overwriting old file
+    f = open(output_filename, 'wb')  # open work file, overwriting old file
     for line_num, line in enumerate(work_file_lined):  # iterate over work file contents
         writeable_line = line
         if writeable_line.startswith("A") and pad_arec == "True":
             writeable_line = writeable_line[0:1] + arec_padding[0:6] + writeable_line[7:]  # write "A" line
-        f.write(writeable_line.replace('\n', "\r\n"))
+        f.write(writeable_line.replace('\n', "\r\n").encode())
     else:
-        f.write(chr(26))
+        f.write(chr(26).encode())
 
     f.close()  # close output file
