a79a9ed1d6fa7fedd6c448fca98a51dddbd4e6bc
dtg01100 <dlafreniere349@gmail.com>
2016-06-29 16:20:07 -0400

    fix encoding error in error reporting module

--- record_error.py
+++ record_error.py
@@ -7,6 +7,6 @@ def do(run_log, errors_log, error_message, filename, error_source):
     message = "At: " + str(time.ctime()) + "\r\n" + "From module: " + error_source + "\r\n" + "For object: " +\
               filename + "\r\n" + "Error Message is:" + "\r\n" + (str(error_message) + "\r\n\r\n")
     # record error to both the run log and the errors log
-    run_log.write(message)
+    run_log.write(message.encode())
     errors_log.write(message)
     print(message)
