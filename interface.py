1aa83ad2492b0fc58ff544a9f0db6d9f09bffb8e
dtg01100 <dlafreniere349@gmail.com>
2016-08-08 13:57:54 -0400

    create 1.18 release branch, set version number in version string

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ import platform
 
 # initialize gui here to show something as early as possible
 appname = "Batch File Sender"
-version = "(Git Branch: Master)"
+version = "1.18.0"
 database_version = "14"
 print(appname + " Version " + version)
 running_platform = platform.system()
