527a5301a19cab4e8dd9bd8abb0c45cb8d80aaf3
dtg01100 <dlafreniere349@gmail.com>
2017-01-23 08:18:02 -0500

    change version string to git master

--- interface.py
+++ interface.py
@@ -32,7 +32,7 @@ from operator import itemgetter
 from tendo import singleton
 
 appname = "Batch File Sender"
-version = "1.20.0"
+version = "(Git Branch: Master)"
 database_version = "15"
 print(appname + " Version " + version)
 running_platform = platform.system()
