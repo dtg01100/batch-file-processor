fb20bf499b93ca842ea4b2a2461ca32be6d3a9cc
dtg01100 <dlafreniere349@gmail.com>
2018-08-10 09:40:13 -0400

    version bump for bug fix

--- interface.py
+++ interface.py
@@ -34,7 +34,7 @@ from operator import itemgetter
 from tendo import singleton
 
 appname = "Batch File Sender"
-version = "1.23.0"
+version = "1.23.1"
 database_version = "16"
 print(appname + " Version " + version)
 running_platform = platform.system()
