03b3c472790017a3d5d004ef4b1499051de51c47
David LaFreniere <dlafreniere349@gmail.com>
2022-02-10 16:22:11 -0500

    version for release

--- interface.py
+++ interface.py
@@ -40,7 +40,7 @@ import traceback
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     appname = "Batch File Sender"
-    version = "(Git Branch: Master)"
+    version = "1.30.0"
     database_version = "19"
     print(appname + " Version " + version)
     running_platform = platform.system()
