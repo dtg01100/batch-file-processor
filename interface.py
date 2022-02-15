fb49ebce3a51ac09b0a180d4bf5ca6a0967bb47b
David LaFreniere <dlafreniere349@gmail.com>
2022-02-15 15:22:48 -0500

    version bump for bugfix

--- interface.py
+++ interface.py
@@ -40,7 +40,7 @@ import traceback
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     appname = "Batch File Sender"
-    version = "1.30.0"
+    version = "1.30.1"
     database_version = "19"
     print(appname + " Version " + version)
     running_platform = platform.system()
