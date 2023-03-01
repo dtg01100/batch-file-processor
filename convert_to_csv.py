830613d9c23b62e661354874c428e38815988745
David LaFreniere <dlafreniere349@gmail.com>
2023-03-01 13:42:27 -0500

    partial rewrite of convert_to_csv  use builtin csv module

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,3 +1,4 @@
+import csv
 import upc_e_to_upc_a
 import line_from_mtc_edi_to_dict
 from decimal import Decimal
@@ -26,101 +27,101 @@ def edi_convert(edi_process, output_filename, each_upc_lut, parameters_dict):
 
     with open(edi_process) as work_file:  # open input file
         work_file_lined = [n for n in work_file.readlines()]  # make list of lines
-        f = open(output_filename, 'wb')  # open work file, overwriting old file
+        with open(output_filename, "w", newline="") as f:  # open work file, overwriting old file
+            csv_file = csv.writer(f, dialect="excel", lineterminator="\r\n", quoting=csv.QUOTE_ALL)
 
-        if conv_inc_headers != "False":  # include headers if flag is set
-            # write line out to file
-            f.write("{}" "," "{}" "," "{}" "," "{}" "," "{}" "," "{}" "," "{}\r\n"
-                    .format("UPC", "Qty. Shipped", "Cost", "Suggested Retail",
-                            "Description", "Case Pack", "Item Number").encode())
+            if conv_inc_headers != "False":  # include headers if flag is set
+                # write line out to file
+                csv_file.writerow(["UPC", "Qty. Shipped", "Cost", "Suggested Retail",
+                                "Description", "Case Pack", "Item Number"])
 
-        for line_num, line in enumerate(work_file_lined):  # iterate over work file contents
-            input_edi_dict = line_from_mtc_edi_to_dict.capture_records(line)
+            for line_num, line in enumerate(work_file_lined):  # iterate over work file contents
+                input_edi_dict = line_from_mtc_edi_to_dict.capture_records(line)
+                if input_edi_dict is not None:
 
-            # if include "A" records flag is set and line starts with "A"
-            if line.startswith("A") and conv_inc_arec != "False":
-                # write "A" line
-                f.write(((line[0:1] + arec_padding[0:6] + line[
-                                                          7:]).rstrip() + "\r\n").encode()) if pad_arec == "True" \
-                    else f.write((line.rstrip() + "\r\n").encode())
+                    # if include "A" records flag is set and line starts with "A"
+                    if input_edi_dict['record_type'] == "A" and conv_inc_arec != "False":
+                        # write "A" line
+                        if pad_arec == "True":
+                            cust_vendor = arec_padding
+                        else:
+                            cust_vendor = input_edi_dict['cust_vendor'] 
+                        csv_file.writerow([input_edi_dict['record_type'], cust_vendor, input_edi_dict['invoice_number'], input_edi_dict['invoice_date'], input_edi_dict['invoice_total']])
 
-            # the following block writes "B" lines, dependent on filter and convert settings
-            # ternary conditional operator: puts if-then-else statement in one line
-            # syntax: <expression1> if <condition> else <expression2>
-            # needs to be wrapped an parenthesis to separate statements
+                    # the following block writes "B" lines, dependent on filter and convert settings
+                    # ternary conditional operator: puts if-then-else statement in one line
+                    # syntax: <expression1> if <condition> else <expression2>
+                    # needs to be wrapped an parenthesis to separate statements
 
-            if line.startswith("B"):
-                if retail_uom:
-                    edi_line_pass = False
-                    try:
-                        item_number = int(input_edi_dict['vendor_item'].strip())
-                        float(input_edi_dict['unit_cost'].strip())
-                        test_unit_multiplier = int(input_edi_dict['unit_multiplier'].strip())
-                        if test_unit_multiplier == 0:
-                            raise ValueError
-                        int(input_edi_dict['qty_of_units'].strip())
-                        edi_line_pass = True
-                    except Exception:
-                        print("cannot parse b record field, skipping")
-                    if edi_line_pass:
+                    if input_edi_dict['record_type'] == "B":
+                        if retail_uom:
+                            edi_line_pass = False
+                            try:
+                                item_number = int(input_edi_dict['vendor_item'].strip())
+                                float(input_edi_dict['unit_cost'].strip())
+                                test_unit_multiplier = int(input_edi_dict['unit_multiplier'].strip())
+                                if test_unit_multiplier == 0:
+                                    raise ValueError
+                                int(input_edi_dict['qty_of_units'].strip())
+                                edi_line_pass = True
+                            except Exception:
+                                print("cannot parse b record field, skipping")
+                            if edi_line_pass:
+                                try:
+                                    each_upc_string = each_upc_lut[item_number][:11].ljust(11)
+                                except KeyError:
+                                    each_upc_string = "           "
+                                try:
+                                    input_edi_dict["unit_cost"] = str(Decimal((Decimal(input_edi_dict['unit_cost'].strip()) / 100) / Decimal(input_edi_dict['unit_multiplier'].strip())).quantize(Decimal('.01'))).replace(".", "")[-6:].rjust(6,'0')
+                                    input_edi_dict['qty_of_units'] = str(int(input_edi_dict['unit_multiplier'].strip()) * int(input_edi_dict['qty_of_units'].strip())).rjust(5,'0')
+                                    input_edi_dict['upc_number'] = each_upc_string
+                                    input_edi_dict['unit_multiplier'] = '000001'
+                                except Exception as error:
+                                    print(error)
+                        blank_upc = False
+                        upc_string = ""
+                        if force_each_upc:
+                            try:
+                                input_edi_dict['upc_number'] = each_upc_lut[int(input_edi_dict['vendor_item'].strip())]
+                            except KeyError:
+                                input_edi_dict['upc_number'] = ""
                         try:
-                            each_upc_string = each_upc_lut[item_number][:11].ljust(11)
-                        except KeyError:
-                            each_upc_string = "           "
-                        try:
-                            input_edi_dict["unit_cost"] = str(Decimal((Decimal(input_edi_dict['unit_cost'].strip()) / 100) / Decimal(input_edi_dict['unit_multiplier'].strip())).quantize(Decimal('.01'))).replace(".", "")[-6:].rjust(6,'0')
-                            input_edi_dict['qty_of_units'] = str(int(input_edi_dict['unit_multiplier'].strip()) * int(input_edi_dict['qty_of_units'].strip())).rjust(5,'0')
-                            input_edi_dict['upc_number'] = each_upc_string
-                            input_edi_dict['unit_multiplier'] = '000001'
-                        except Exception as error:
-                            print(error)
-                blank_upc = False
-                upc_string = ""
-                if force_each_upc:
-                    try:
-                        input_edi_dict['upc_number'] = each_upc_lut[int(input_edi_dict['vendor_item'].strip())]
-                    except KeyError:
-                        input_edi_dict['upc_number'] = ""
-                try:
-                    _ = int(input_edi_dict['upc_number'].rstrip())
-                except ValueError:
-                    blank_upc = True
+                            _ = int(input_edi_dict['upc_number'].rstrip())
+                        except ValueError:
+                            blank_upc = True
 
-                if blank_upc is False:
-                    proposed_upc = input_edi_dict['upc_number'].strip()
-                    if len(str(proposed_upc)) == 11:
-                        upc_string = str(proposed_upc) + str(upc_e_to_upc_a.calc_check_digit(proposed_upc))
-                    else:
-                        if len(str(proposed_upc)) == 8:
-                            upc_string = str(upc_e_to_upc_a.convert_UPCE_to_UPCA(proposed_upc))
+                        if blank_upc is False:
+                            proposed_upc = input_edi_dict['upc_number'].strip()
+                            if len(str(proposed_upc)) == 11:
+                                upc_string = str(proposed_upc) + str(upc_e_to_upc_a.calc_check_digit(proposed_upc))
+                            else:
+                                if len(str(proposed_upc)) == 8:
+                                    upc_string = str(upc_e_to_upc_a.convert_UPCE_to_UPCA(proposed_upc))
 
-                upc_in_csv = "\t" + upc_string if conv_calc_upc != "False" and blank_upc is False \
-                    else input_edi_dict['upc_number']
+                        upc_in_csv = "\t" + upc_string if conv_calc_upc != "False" and blank_upc is False \
+                            else input_edi_dict['upc_number']
 
-                quantity_shipped_in_csv = input_edi_dict['qty_of_units'].lstrip("0") \
-                    if not input_edi_dict['qty_of_units'].lstrip("0") == "" else input_edi_dict['qty_of_units']
+                        quantity_shipped_in_csv = input_edi_dict['qty_of_units'].lstrip("0") \
+                            if not input_edi_dict['qty_of_units'].lstrip("0") == "" else input_edi_dict['qty_of_units']
 
-                cost_in_csv = convert_to_price(input_edi_dict['unit_cost'])
+                        cost_in_csv = convert_to_price(input_edi_dict['unit_cost'])
 
-                suggested_retail_in_csv = convert_to_price(input_edi_dict['suggested_retail_price'])
+                        suggested_retail_in_csv = convert_to_price(input_edi_dict['suggested_retail_price'])
 
-                description_in_csv = (input_edi_dict['description'].replace("&", "AND").rstrip(" ")
-                                      if filter_ampersand != "False" else
-                                      input_edi_dict['description'].rstrip(" "))
+                        description_in_csv = (input_edi_dict['description'].replace("&", "AND").rstrip(" ")
+                                            if filter_ampersand != "False" else
+                                            input_edi_dict['description'].rstrip(" "))
 
-                case_pack_in_csv = input_edi_dict['unit_multiplier'].lstrip("0") \
-                    if not input_edi_dict['unit_multiplier'].lstrip("0") == "" else input_edi_dict['unit_multiplier']
+                        case_pack_in_csv = input_edi_dict['unit_multiplier'].lstrip("0") \
+                            if not input_edi_dict['unit_multiplier'].lstrip("0") == "" else input_edi_dict['unit_multiplier']
 
-                item_number_in_csv = input_edi_dict['vendor_item'].lstrip("0") \
-                    if not input_edi_dict['vendor_item'].lstrip("0") == "" else input_edi_dict['vendor_item']
+                        item_number_in_csv = input_edi_dict['vendor_item'].lstrip("0") \
+                            if not input_edi_dict['vendor_item'].lstrip("0") == "" else input_edi_dict['vendor_item']
 
-                f.write(
-                    '"'"{}"'"'","'"'"{}"'"'","'"'"{}"'"'","'"'"{}"'"'","'"'"{}"'"'","'"'"{}"'"'","'"'"{}"'"'"\r\n".
-                    format(upc_in_csv, quantity_shipped_in_csv, cost_in_csv, suggested_retail_in_csv,
-                           description_in_csv, case_pack_in_csv, item_number_in_csv).encode())
+                        csv_file.writerow([upc_in_csv, quantity_shipped_in_csv, cost_in_csv, suggested_retail_in_csv,
+                                description_in_csv, case_pack_in_csv, item_number_in_csv])
 
-            # if include "C" records flag is set and line starts with "C"
-            if line.startswith("C") and conv_inc_crec != "False":
-                f.write((line.rstrip() + "\r\n").encode())  # write "C" line
+                    # if include "C" records flag is set and line starts with "C"
 
-        f.close()  # close output file
+                    if line.startswith("C") and conv_inc_crec != "False":
+                        csv_file.writerow([input_edi_dict['record_type'], input_edi_dict['charge_type'], input_edi_dict['description'], input_edi_dict['amount']])
