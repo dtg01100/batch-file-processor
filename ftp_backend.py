acf51d6605dd5f12a56960dd1679f153df39c401
dtg01100 <dlafreniere349@gmail.com>
2024-08-19 14:45:06 -0400

    add support for ftps

--- ftp_backend.py
+++ ftp_backend.py
@@ -9,16 +9,26 @@ import os
 def do(process_parameters, filename):
     file_pass = False
     counter = 0
+    ftp_providers = [ftplib.FTP_TLS, ftplib.FTP]
     while not file_pass:
         try:
             with open(filename, 'rb') as send_file:
                 filename_no_path = os.path.basename(filename)
-                ftp = ftplib.FTP()
-                ftp.connect(str(process_parameters['ftp_server']), process_parameters['ftp_port'])
-                ftp.login(process_parameters['ftp_username'], process_parameters['ftp_password'])
-                ftp.storbinary("stor " + process_parameters['ftp_folder']+filename_no_path, send_file)
-                ftp.close()
-                file_pass = True
+                for provider in ftp_providers:
+                    ftp = provider()
+                    try:
+                        print(f"Connecting to ftp server: {str(process_parameters['ftp_server'])}")
+                        ftp.connect(str(process_parameters['ftp_server']), process_parameters['ftp_port'])
+                        print(f"Logging in to {str(process_parameters['ftp_server'])}")
+                        ftp.login(process_parameters['ftp_username'], process_parameters['ftp_password'])
+                        print("Sending File...")
+                        ftp.storbinary("stor " + process_parameters['ftp_folder']+filename_no_path, send_file)
+                        print("Success")
+                        ftp.close()
+                        file_pass = True
+                        break
+                    except Exception:
+                        print("Falling back to non-TLS...")
 
         except Exception as ftp_error:
             if counter == 10:
