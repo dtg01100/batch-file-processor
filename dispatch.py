4c4b03a87be4cd9389501aa91d23f881a48a3d21
David LaFreniere <dlafreniere349@gmail.com>
2025-04-30 14:17:15 +0000

    rename variable

--- dispatch.py
+++ dispatch.py
@@ -389,7 +389,7 @@ def process(database_connection, folders_database, run_log, emails_table, run_lo
                                             print("Applying tweaks to " + output_send_filename)
                                             process_files_log.append(("Applying tweaks to " + output_send_filename + "\r\n\r\n"))
                                             output_send_filename = edi_tweaks.edi_tweak(
-                                                output_send_filename, output_filename, each_upc_dict,
+                                                output_send_filename, output_filename, upc_dict,
                                                 parameters_dict, settings)
                                             print("Success")
                                             process_files_log.append("Success\r\n\r\n")
