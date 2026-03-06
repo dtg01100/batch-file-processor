65e887d9a18596015f6ec4070933ca144a1ed86c
David LaFreniere <dlafreniere349@gmail.com>
2026-03-06 16:00:11 -0500

    build(windows): remove redundant PyQt6 hooks
    These hooks were conflicting with PyInstaller's built-in hooks,
    causing missing Qt plugins and platform DLLs in the final bundle.

--- hooks/hook-PyQt6.QtWidgets.py
+++
@@ -1,5 +0,0 @@
-# Hook for PyQt6.QtWidgets
-from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
-
-datas = collect_data_files('PyQt6.QtWidgets')
-binaries = collect_dynamic_libs('PyQt6.QtWidgets')
