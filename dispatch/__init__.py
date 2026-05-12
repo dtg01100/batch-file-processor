8d5dfb30f872e2cf6ceca2944bcb6648aebd9540
David LaFreniere <dlafreniere349@gmail.com>
2026-05-12 11:47:49 -0400

    chore: remove legacy/dead code and stale exports
    Delete deprecated modules that have been superseded:
    - backend/retry_mixin.py (unused retry mixin)
    - dispatch/config/ (replaced by config_builder)
    - dispatch/legacy_process.py (replaced by orchestrator)
    - dispatch/services/file_processing_helpers.py (unused helpers)
    - interface/services/smtp_alert_service.py (unused)
    - migrations/add_audit_log_table.py (stale migration)
    - Corresponding test files for all of the above
    
    Clean up dispatch/__init__.py re-exports that reference deleted modules.

--- dispatch/__init__.py
+++ dispatch/__init__.py
@@ -7,89 +7,10 @@ Feature Flags:
     DISPATCH_DEBUG_MODE: Set to 'true' for verbose debug logging
 """
 
-from dispatch.edi_validator import EDIValidator
-from dispatch.error_handler import ErrorHandler
 from dispatch.feature_flags import get_debug_mode, get_feature_flags, set_feature_flag
-from dispatch.file_utils import build_output_filename, do_clear_old_files
-from dispatch.hash_utils import generate_file_hash, generate_match_lists
-from dispatch.interfaces import BackendInterface, DatabaseInterface, FileSystemInterface
-from dispatch.legacy_process import process
-from dispatch.log_sender import (
-    EmailConfig,
-    LogEntry,
-    LogSender,
-    MockEmailService,
-    MockUIService,
-    NullUIService,
-    SMTPEmailService,
-)
-from dispatch.orchestrator import DispatchOrchestrator
-from dispatch.preflight_validator import (
-    PreflightIssue,
-    PreflightResult,
-    PreflightValidator,
-)
-from dispatch.print_service import (
-    MockPrintService,
-    NullPrintService,
-    PrintServiceProtocol,
-    RunLogPrinter,
-    UnixPrintService,
-    WindowsPrintService,
-)
-from dispatch.processed_files_tracker import (
-    InMemoryDatabase,
-    ProcessedFileRecord,
-    ProcessedFilesTracker,
-)
-from dispatch.results import DispatchConfig, FolderResult
-from dispatch.send_manager import SendManager
 
 __all__ = [
-    "BackendInterface",
-    # Interfaces
-    "DatabaseInterface",
-    "DispatchConfig",
-    "DispatchOrchestrator",
-    # Components
-    "EDIValidator",
-    # Log Sender
-    "EmailConfig",
-    "ErrorHandler",
-    "FileSystemInterface",
-    "FolderResult",
-    "InMemoryDatabase",
-    "LogEntry",
-    "LogSender",
-    "MockEmailService",
-    "MockPrintService",
-    "MockUIService",
-    "NullPrintService",
-    "NullUIService",
-    "PreflightIssue",
-    "PreflightResult",
-    # Preflight Validation
-    "PreflightValidator",
-    # Print Service
-    "PrintServiceProtocol",
-    # Processed Files Tracker
-    "ProcessedFileRecord",
-    "ProcessedFilesTracker",
-    "RunLogPrinter",
-    "SMTPEmailService",
-    "SendManager",
-    "UnixPrintService",
-    "WindowsPrintService",
-    # File utilities
-    "build_output_filename",
-    "do_clear_old_files",
-    "generate_file_hash",
-    # Hash utilities
-    "generate_match_lists",
-    # Feature Flags
     "get_debug_mode",
     "get_feature_flags",
-    # Legacy process function
-    "process",
     "set_feature_flag",
 ]
